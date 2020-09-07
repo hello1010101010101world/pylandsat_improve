3b29d5506d66e878f752a943bf528c1eb7b66566
hello1010101010101world <31617925+hello1010101010101world@users.noreply.github.com>
2020-09-07 14:55:57 +0800

    Add files via upload

---
+++ mydownload.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+# @Author : qtwang
+# @Email : qtwang315518@163.com
+# @Time : 2020-9-2 14:42
+#https://github.com/yannforget/pylandsat
+from datetime import datetime
+
+from shapely.geometry import Point
+from pylandsat import Catalog, Product
+
+catalog = Catalog()
+
+begin = datetime(2020, 7, 20)
+end = datetime(2020, 9, 4)
+path=[121,123,118,119,125,120,120,122]
+row=[44,45,41,42,45,32,36,45]
+# geom = Point(4.34, 50.85)
+
+# Results are returned as a list
+# scenes = catalog.search(
+#     begin=begin,
+#     end=end,
+#     geom=geom,
+#     sensors=['ETM', 'LC08']
+# )
+
+scenes = catalog.search(begin=begin,end=end,path=path,row =row,sensors=['LE07', 'LC08'],maxcloud=100)
+print("共查询到%s景数据,即将开始下载！"%(len(scenes)))
+for scene in scenes:
+    product_id = scene['product_id']
+    # Download the scene
+    product = Product(product_id)
+    product.download(out_dir='data')
